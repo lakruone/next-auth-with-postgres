633ecca807162bd29c98e4f6645a81a57aaf3ed9
lakruone <lakruone@gmail.com>
2024-07-07 12:25:31 +0530

    eslint issue fixed

--- src/app/users/error.tsx
+++ src/app/users/error.tsx
@@ -1,14 +1,14 @@
 "use client"
 import { useEffect } from "react"
 
-const error = ({ error }: { error: Error }) => {
-    useEffect(()=> {
-        console.log(error.message);
+const Error = ({ error }: { error: Error }) => {
+    // useEffect(()=> {
+    //     console.log(error.message);
         
-    },[error])
+    // },[error])
   return (
     <div>error: {error.message}</div>
   )
 }
 
-export default error
+export default Error
