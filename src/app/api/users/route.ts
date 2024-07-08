6e61a3164670d5957f3be146133c19ca21317fee
lakruone <lakruone@gmail.com>
2024-07-08 16:47:17 +0530

    prisma model added

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,12 @@
+
+
+export async function GET(req: Request, res: Response) {
+  // const { messages } = await req.json();
+
+  // const result = await streamText({
+  //   model: openai('gpt-4-turbo'),
+  //   messages,
+  // });
+
+  // return result.toAIStreamResponse();
+}
